ec3a31979d792f960dd98fd9edbd836659dda8f5
Sean B <strawhorse@gmail.com>
2023-07-19 19:55:51 +0100

    Create assignment_list.php
    Setting up main file to display main view

---
+++ view/assignment_list.php
@@ -0,0 +1,20 @@
+<!-- main template for the application -->
+
+<?php include ('view/header.php'); ?>
+
+
+
+<section id="list" class="list>
+
+    <header class="list__row list__header">
+        <h1>Assignments</h1>
+
+    </section>
+
+
+
+
+
+
+
+<?php include ('view/footer.php'); ?>
