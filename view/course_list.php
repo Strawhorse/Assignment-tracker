9946212648759e27d36e33a2678cf2e84b6a327a
Sean B <strawhorse@gmail.com>
2023-07-27 11:39:06 +0100

    Creating course list with view, add, and delete functions

--- view/course_list.php
+++ view/course_list.php
@@ -1,10 +1,33 @@
-<!-- list all courses and be able to list a course -->
+<!-- list all courses and be able to add a course -->
 
 
 <?php include('view/header.php') ?>
 
 
-<!-- 55 mins -->
+<?php if ($courses) { ?>
+
+    <section id="list" class="list">
+        <header class="list__row list__header">
+            <h1>Course List</h1>
+        </header>
+
+
+        <?php foreach($courses as $course) : ?>
+            <div class="list__row"></div>
+                <div class="list__item">
+                    <p class="bold"><?= $course['courseName'] ?></p>
+                </div>
+
+                <div class="list_removeItem">
+                    <form action="." method="post">
+                        <input type="hidden" name="action" value="delete_course">
+                        <input type="hidden" name="course_id" value="<?= $course['courseID']?>"
+                    </form>
+                </div>
+    <?php } else { ?>
+        <p>No courses exist</p>
+    <?php } ?>
+
 
 
 
