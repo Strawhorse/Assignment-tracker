e004d8f50e99c5f19137b8389a0e396ba38de69e
Sean B <strawhorse@gmail.com>
2023-07-27 11:53:01 +0100

    Completed section of just html to add a course

--- view/course_list.php
+++ view/course_list.php
@@ -21,14 +21,37 @@
                 <div class="list_removeItem">
                     <form action="." method="post">
                         <input type="hidden" name="action" value="delete_course">
-                        <input type="hidden" name="course_id" value="<?= $course['courseID']?>"
+                        <input type="hidden" name="course_id" value="<?= $course['courseID']?>">
+                        <button class="remove-button">❌</button>
                     </form>
                 </div>
+            </div>
+
+            <?php endforeach ?>
+    </section>
+            <!-- end of loop through courses -->
     <?php } else { ?>
         <p>No courses exist</p>
     <?php } ?>
 
 
+    <!-- adding a course -->
+    <section id="add" class="add">
+        <h2>Add course</h2>
+        <form action="." method="post" id="add__form" class="add__form">
+            <input type="hidden" name="action" value="add__course">
+            <div class="add__inputs">
+                <label for="">Name:</label>
+                <input type="text" name="course_name" maxlength="50" placeholder="Name" autofocus required>
+            </div>
+
+            <div class="add_addItem">
+                <button class="add-button bold">Add</button>
+            </div>
+        </form>
+    </section>
+    <br>
+    <p><a href=".">View &amp; Add Assignments</a></p>
 
 
 
