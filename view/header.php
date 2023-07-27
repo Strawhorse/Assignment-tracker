1d790b2b7791f9b16870e620a33ed54b67767ea5
Sean B <strawhorse@gmail.com>
2023-07-27 12:24:56 +0100

    Setting us sass files and checking pat hs

--- view/header.php
+++ view/header.php
@@ -3,6 +3,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Assignment tracker</title>
+
+
+    <!-- location of sass files -->
     <link rel="stylesheet" href="view/css/main.min.css" />
 </head>
 <body>
